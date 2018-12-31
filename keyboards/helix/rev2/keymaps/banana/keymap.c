a9917e2ad1f14d653cdeff22d01ff5c19895f226
Banana Shimakawa <shimakawa@c-fo.com>
2018-12-31 12:31:57 +0900

    met keys

--- keyboards/helix/rev2/keymaps/banana/keymap.c
+++ keyboards/helix/rev2/keymaps/banana/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Qwerty
    * ,-----------------------------------------.             ,-----------------------------------------.
-   * |   `  |   1  |   2  |   3  |Lower |Enter |             |Enter |Raise |   8  |   9  |   0  | Del  |
+   * | Esc  |   `  |   2  |   3  |Lower |Enter |             |Enter |Raise |   *  |   [  |   ]  | Del  |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Tab  |   Q  |   W  |   E  |   R  |   T  |             |   Y  |   U  |   I  |   O  |   P  | Bksp |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
@@ -70,15 +70,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |   [  |   ]  |   N  |   M  |   ,  |   .  |   /  |Shift |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |Adjust| Esc  | Alt  | GUI  | EISU |Enter |Space |Space |Enter | KANA | Left | Down |  Up  |Right |
+   * |Adjust| Esc  | Alt  | Alt  | GUI  |Space |Space |Space |Space | GUI  |   Up | Down | Left |Right |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT( \
-      KC_GRV,  KC_1,    KC_2,    KC_3,    LOWER,   KC_ENT,                    KC_ENT,  RAISE,   KC_8,    KC_9,    KC_0,    KC_DEL, \
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+      KC_ESC,  KC_GRV,  KC_2,    KC_3,    LOWER,   KC_ENT,                    KC_ENT,  RAISE,   KC_ASTR, KC_LBRC, KC_RBRC, KC_DEL,   \
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,  \
+      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT , \
-      ADJUST,  KC_ESC,  KC_LALT, KC_LGUI, EISU,    KC_ENT,  KC_SPC,  KC_SPC,  KC_ENT,  KANA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+      ADJUST,  KC_ESC,  KC_LALT, KC_LALT, KC_LGUI, KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  KC_LGUI, KC_UP,   KC_DOWN, KC_LEFT,   KC_RGHT   \
       ),
 
   /* Colemak
