1e4467aae44f0d19e63546e6eb00609013359141
Banana Shimakawa <shimakawa@c-fo.com>
2019-01-02 20:56:43 +0900

    RGUI

--- keyboards/helix/rev2/keymaps/bananaumai/keymap.c
+++ keyboards/helix/rev2/keymaps/bananaumai/keymap.c
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,  \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LPRN, KC_RPRN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT , \
-      ADJUST,  KC_PGUP, KC_PGDN, KC_LALT, KC_LGUI, KC_SPC,  RAISE,   LOWER,  KC_SPC,  KC_LGUI, KC_UP,   KC_DOWN, KC_LEFT,   KC_RGHT   \
+      ADJUST,  KC_PGUP, KC_PGDN, KC_LALT, KC_LGUI, KC_SPC,  RAISE,   LOWER,   KC_SPC,  KC_RGUI, KC_UP,   KC_DOWN, KC_LEFT,   KC_RGHT   \
       ),
 
   /* Colemak
