317f10282a37534fc14df6bcff9e06e6637184f8
Banana Shimakawa <shimakawa@c-fo.com>
2019-01-03 11:16:32 +0900

    equal

--- keyboards/helix/rev2/keymaps/bananaumai/keymap.c
+++ keyboards/helix/rev2/keymaps/bananaumai/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Qwerty
    * ,-----------------------------------------.             ,-----------------------------------------.
-   * | Esc  |   `  |   \  |   -  |Lower |Enter |             |Enter |Raise |   +  |   [  |   ]  | Del  |
+   * | Esc  |   `  |   \  |   -  |Lower |Enter |             |Enter |Raise |   =  |   [  |   ]  | Del  |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Tab  |   Q  |   W  |   E  |   R  |   T  |             |   Y  |   U  |   I  |   O  |   P  | Bksp |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT( \
-      KC_ESC,  KC_GRV,  KC_BSLS, KC_MINS, LOWER,   KC_ENT,                    KC_ENT,  RAISE,   KC_PLUS, KC_LBRC, KC_RBRC, KC_DEL,   \
+      KC_ESC,  KC_GRV,  KC_BSLS, KC_MINS, LOWER,   KC_ENT,                    KC_ENT,  RAISE,   KC_EQL,  KC_LBRC, KC_RBRC, KC_DEL,   \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,  \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LPRN, KC_RPRN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT , \
