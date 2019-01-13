830ce641c349d59b7122baf79167042d07c63cc1
島川悠太 <yuta@bnn-mac-mini.local>
2019-01-14 00:45:50 +0900

    bksp with thumb

--- keyboards/helix/rev2/keymaps/bananaumai/keymap.c
+++ keyboards/helix/rev2/keymaps/bananaumai/keymap.c
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |   [  |   ]  |   N  |   M  |   ,  |   .  |   /  |Shift |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * | Esc  |  Up  | Down | Alt  | GUI  |Space |Raise |LOWER |Space | GUI  | Enter| Left | Right| Bksp |
+   * | Esc  |  Up  | Down | Alt  | GUI  |Space |Raise |LOWER | Bksp | Gui  | Enter| Left |Right | Del  |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT( \
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,  \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT , \
-      KC_ESC,  KC_UP,   KC_DOWN, KC_LALT, KC_LGUI, KC_SPC,  RAISE,   LOWER,   KC_SPC,  KC_RGUI, KC_ENT,  KC_LEFT, KC_RGHT, KC_BSPC   \
+      KC_ESC,  KC_UP,   KC_DOWN, KC_LALT, KC_LGUI, KC_SPC,  RAISE,   LOWER,   KC_BSPC, KC_RGUI, KC_ENT,  KC_LEFT, KC_RGHT, KC_DEL    \
       ),
 
   /* Colemak
