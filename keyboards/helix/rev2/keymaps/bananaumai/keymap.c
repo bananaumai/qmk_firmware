f48f6d15d2e49fde726b8d0182f36c36ec0ee312
Banana Shimakawa <shimakawa@c-fo.com>
2019-01-21 16:40:10 +0900

    GUI/GUI

--- keyboards/helix/rev2/keymaps/bananaumai/keymap.c
+++ keyboards/helix/rev2/keymaps/bananaumai/keymap.c
@@ -68,17 +68,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Ctrl |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  |  '   |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * | Shift|   Z  |   X  |   C  |   V  |   B  |   [  |   ]  |   N  |   M  |   ,  |   .  |   /  |Shift |
+   * | Shift|   Z  |   X  |   C  |   V  |   B  |Space | Bksp |   N  |   M  |   ,  |   .  |   /  |Shift |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * | Esc  |  Up  | Down | Alt  | GUI  |Space |Raise |LOWER | Bksp | Gui  | Enter| Left |Right | Del  |
+   * | Esc  |  Up  | Down | Alt  | Gui  | Gui  |Raise |LOWER |  Gui |  Gui | Enter| Left |Right | Del  |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT( \
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,  \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  \
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT , \
-      KC_ESC,  KC_UP,   KC_DOWN, KC_LALT, KC_LGUI, KC_SPC,  RAISE,   LOWER,   KC_BSPC, KC_RGUI, KC_ENT,  KC_LEFT, KC_RGHT, KC_DEL    \
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC,  KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT , \
+      KC_ESC,  KC_UP,   KC_DOWN, KC_LALT, KC_LGUI, KC_LGUI, RAISE,   LOWER,   KC_RGUI, KC_RGUI, KC_ENT,  KC_LEFT, KC_RGHT, KC_DEL    \
       ),
 
   /* Colemak
